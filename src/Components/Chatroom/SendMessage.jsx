c684159ba2d2ecb73327ad7d4a66185f4704672d
roshan-42 <roshanacharya207@gmail.com>
2024-05-17 20:51:11 +0545

    on enter message send added

--- src/Components/Chatroom/SendMessage.jsx
+++ src/Components/Chatroom/SendMessage.jsx
@@ -38,21 +38,26 @@ const SendMessage = () => {
   };
   return (
     <div className="flex items-center mt-4">
-      <input
-        onChange={(e) => setMessage(e.target.value)}
-        type="text"
-        value={message}
-        placeholder="Type your message"
-        className="flex-grow border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:border-blue-500"
-      />
-      <button
-        onClick={(e) => {
+      <form
+        className="w-full flex"
+        onSubmit={(e) => {
           SendMessage(e);
         }}
-        className="ml-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg"
       >
-        Send
-      </button>
+        <input
+          onChange={(e) => setMessage(e.target.value)}
+          type="text"
+          value={message}
+          placeholder="Type your message"
+          className="w-full flex-grow border border-gray-300 rounded-lg  py-2 px-4 focus:outline-none focus:border-blue-500"
+        />
+        <button
+          type="submit"
+          className="ml-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg"
+        >
+          Send
+        </button>
+      </form>
     </div>
   );
 };
